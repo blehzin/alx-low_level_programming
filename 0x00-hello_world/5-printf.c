40958bfa490e7a5196259a245aab50d5c3b9fdaf
ediawe_blessing <ediaweblessing16@gmail.com>
2023-02-16 17:46:17 -0800

    6-size.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main- c program that prints a line using the printf function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammer, but the outcome ia a piece of art,\n");
+return (0);
+}	
