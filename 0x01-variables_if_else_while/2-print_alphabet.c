d1dc204a7368ef91c379cd8e2c3c090b45f11b17
ediawe_blessing <ediaweblessing16@gmail.com>
2023-02-19 06:55:49 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - Prints the alphabet
+ * main - printa alphabet in lower case
  *
  * Return: always 0
  */
 int main(void)
 {
-	char alp[26] - "abcdefghijklmnopqrstuvwxyz";
-	int i;
+	char alphabet = 'a';
 
-	for (i = 0; i < 26; i++)
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 	{
-		putchar(alp[i]);
+		putchar(alphabet);
 	}
-	putchar("\n");
+	putchar('\n');
 	return (0);
 }
