88ee215bac37604154ba370fc0a2ccc87371141c
ediawe_blessing <ediaweblessing16@gmail.com>
2023-02-19 07:58:57 -0800

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -8,11 +8,10 @@
  */
 int main(void)
 {
-	int num;
+	int i;
 
-	for (num = '0'; num <= '9'; num++)
-	{
-		putchar('\n');
-	}
+	for (i = 0 ; i <= 9 ; i++)
+		putchar(i + '0');
+	putchar('\n');
 	return (0);
 }
