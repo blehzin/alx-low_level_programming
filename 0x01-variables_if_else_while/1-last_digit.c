d3c5cf4c39d30ed46c96c2e005069f834f6fb722
ediawe_blessing <ediaweblessing16@gmail.com>
2023-02-19 07:19:09 -0800

    3-print_alphabets.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -6,24 +6,23 @@
 /**
  * main - prints the last digit of n and prints out whether it is
  * greater than 5, 0, or less than 6
- *
  * Return: Always 0 (success)
  */
 int main(void)
 {
 	int n;
-	int m;
+	int Last digit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	m = n % 10;
-	if (m > 5)
-		printf("Last digit of %d is %d and is greater than 5", n, m);
-	else if (m == 0)
-		printf("Last digit of %d is %d and is 0", n, m);
-	else if
-		printf("Last digit of %d is %d and is less than 6 and not 0", n, m);
-
+	Last digit = n % 10;
+	printf("Last digit of %i is %i and is ", n, Last digit);
+	if (Last digit > 5)
+		printf("greater than 5\n");
+	else if (Last digit == 0)
+		printf("0\n");
+	else
+		printf("less than 6 and not 0\n");
 	return (0);
 }
