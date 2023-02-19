8ec0e96be52f4c9bcfaaf8c66deec371af7e2dc0
ediawe_blessing <ediaweblessing16@gmail.com>
2023-02-19 14:38:06 -0800

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,23 +8,34 @@
 
 int main(void)
 {
+	int i1;
+	int j1;
 	int i;
 	int j;
 
-	for (i = 0; i <= 9; i++)
+	for (i1 = 48; i1 < 58; i1++)
 	{
-		for (j = 1; j <= 9; j++)
+		for (i = 48; i < 58; i++)
 		{
-					if ((i < j)
+			j = i + 1;
+			j1 = i1;
+			for (; j1 < 58; j1++)
+			{
+				for (; j < 58; j++)
+				{
+					putchar(i1);
+					putchar(i);
+					putchar(' ');
+					putchar(j1);
+					putchar(j);
+					if (i1 != 57 || j1 != 57 || i != 56 || j != 57)
 					{
-						putchar(i + '0');
-						putchar(j + '0');
-						if (i + j != 18)
-						{
-							putchar(',');
-							putchar(' ');
-						}
+						putchar(',');
+						putchar(' ');
 					}
+				}
+				j = 48;
+			}
 		}
 	}
 	putchar('\n');
