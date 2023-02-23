52038414587c3c11cc6a252b64808d2cb2fd1d87
ediawe_blessing <ediaweblessing16@gmail.com>
2023-02-22 18:41:58 -0800

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -11,9 +11,9 @@ int main(void)
 	sum = 0;
 	for (i = 0; i < 1024; i++)
 	{
-		if ((i % 3 == 0) || (i % 3 == 0))
+		if ((i % 3 == 0) || (i % 5 == 0))
 		{
-			sum = sum + i;
+			sum = sum - i;
 		}
 	}
 	printf("%d\n", sum);
